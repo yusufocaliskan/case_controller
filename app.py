427119b3a239b338bfcfda2cb3b4039a9d7f253c
yusufocaliskan <yusufocaliskan@gmail.com>
2022-05-20 13:54:38 +0300

    Requirements file generated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class Panel(App):
 
     def build(self):
         
-        return Label(text='Silavvvv')
+        return Label(text='Silavvvv Rêz!')
 
 if __name__ == '__main__':
     Panel().run()
